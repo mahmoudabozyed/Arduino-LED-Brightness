6274a473dab6ccb8dad9dfbea501b50ec8d738c6
mahmoudabozyed <146341897+mahmoudabozyed@users.noreply.github.com>
2024-02-05 22:36:16 +0200

    Add files via upload

---
+++ Arduino_LED_Brightness.ino
@@ -0,0 +1,14 @@
+int ledPin = 5;
+int brightness = 0;
+int fadeAmount = 5;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  analogWrite(ledPin, brightness);
+  brightness=brightness+fadeAmount;
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  delay(50);
+}
